2aa3159605af6e814d7f23fe95d950feaa401635
amanda1686 <amanda160986@gmail.com>
2024-04-14 18:20:52 +0200

    feat logo

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,11 +1,35 @@
-import { Navbar } from 'flowbite-react'
+import { Button, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '/img/Logo.png'
+import {AiOutlineSearch} from 'react-icons/ai'
+
 
 function Header() {
   return (
-    <div>
-      <Navbar className=' border-b-2'>Header</Navbar>
-    </div>
+    
+      <Navbar className=' border-b-2'>
+        <Link to="/" className=' self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+          <img 
+          src={Logo}
+          className=' px-2' 
+          alt="logo"
+          width='15%'
+          />
+        </Link>
+        <form>
+          <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className=' hidden lg:inline'
+          />
+        </form>
+        <Button className=' w-12 h-10 lg:hidden' color='gray' pill>
+          <AiOutlineSearch/>
+        </Button>
+      </Navbar>
+    
   )
 }
 
