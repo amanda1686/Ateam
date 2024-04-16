8047d7cf22bc1ba7a6a08325b27a97fc78e91167
amanda1686 <amanda160986@gmail.com>
2024-04-16 20:12:54 +0200

    feat add route and controller for users

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.Route.js';
+
 
 dotenv.config();
 
@@ -16,3 +18,5 @@ const app = express();
 app.listen(3000, () =>{
     console.log('Server is runnig on port 3000');
 });
+
+app.use('/api/user', userRoutes);
