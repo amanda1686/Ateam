f719e8a9de00537a44ca492718961a6b75a0a7ce
amanda1686 <amanda160986@gmail.com>
2024-04-24 22:20:56 +0200

    feat add delete api route

--- server/routes/user.Routes.js
+++ server/routes/user.Routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.Controller.js';
+import { deleteUser, test, updateUser } from '../controllers/user.Controller.js';
 import { verifyToken } from '../utils/veryfyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser );
 
 export default router;
