9e792862fbca7e8d57c1d58d591c1b7eeb6847ef
amanda1686 <amanda160986@gmail.com>
2024-04-25 13:08:57 +0200

    feat show user post inside dashboard

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { verifyToken } from '../utils/veryfyUser.js';
-import { create, getpost } from '../controllers/post.controllers.js';
+import { create, deletepost, getpost, updatepost } from '../controllers/post.controllers.js';
 
 
 const router = express.Router();
 
-router.post('/create', verifyToken, create)
-router.get('/getpost', getpost)
+router.post('/create', verifyToken, create);
+router.get('/getpost', getpost);
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost);
+router.put(`/updatepost/:postId/:userId`, verifyToken, updatepost);
 
 export default router;
