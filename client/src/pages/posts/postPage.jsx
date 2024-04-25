aa7b1b4c7f3762c2042acba5d9c5ea8a7c2255de
amanda1686 <amanda160986@gmail.com>
2024-04-25 08:59:40 +0200

    add post page

--- client/src/pages/posts/postPage.jsx
+++ client/src/pages/posts/postPage.jsx
@@ -81,11 +81,11 @@ if (loading) {
             </div>
             <div className=" p-3 post-content" dangerouslySetInnerHTML={{__html: post.content}}>
             </div>
-            <div className=" max-w-4xl mx-auto w-full">
+            {/* <div className=" max-w-4xl mx-auto w-full">
                 <CallToAction/>
-            </div>
-            <CommentSection postId={post._id}/>
-            <div className="flex flex-col justify-center items-center mb-5">
+            </div> */}
+            {/* <CommentSection postId={post._id}/> */}
+            {/* <div className="flex flex-col justify-center items-center mb-5">
             <h1 className='text-xl mt-5'>Recent Articles</h1>
             <div className="flex flex-col md:flex-row md:w-[1000px] md:gap-4 mt-4">
   {recentPosts && recentPosts.map((post) => (
@@ -93,7 +93,7 @@ if (loading) {
   ))}
 </div>
 
-        </div>
+        </div> */}
         </div>
     );
 }
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utils/veryfyUser.js';
-import { create } from '../controllers/post.controllers.js';
+import { create, getpost } from '../controllers/post.controllers.js';
 
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
-
+router.get('/getpost', getpost)
 
 export default router;
